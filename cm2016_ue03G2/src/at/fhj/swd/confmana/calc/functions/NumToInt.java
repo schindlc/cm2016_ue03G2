d1263ff286c5e4dc7d6c4ac3af89a3fdb4234514
total-recall <josef.steppan@gmail.com>
2016-05-14 09:36:50 +0200

    added comments

--- cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/NumToInt.java
+++ cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/NumToInt.java
@@ -1,6 +1,14 @@
 package at.fhj.swd.confmana.calc.functions;
 
+/**
+ * NumToInt class to convert double to int
+ */
 public class NumToInt {
+	/**
+	 * converts double to int
+	 * @param number double
+	 * @return int
+	 */
 	public int numToInt(double number){
 		// insert meaningful code here and adopt dummy return
 		return new Double(number).intValue();
