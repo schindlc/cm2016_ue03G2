68ae2f4023debf9e60d1ab41670bc6efc765cbc0
total-recall <josef.steppan@gmail.com>
2016-05-14 09:32:58 +0200

    implemented numToInt()

--- cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/NumToInt.java
+++ cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/NumToInt.java
@@ -3,6 +3,6 @@ package at.fhj.swd.confmana.calc.functions;
 public class NumToInt {
 	public int numToInt(double number){
 		// insert meaningful code here and adopt dummy return
-		return 0;
+		return new Double(number).intValue();
 	}
 }
