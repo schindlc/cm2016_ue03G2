1e8a2e350a18efe0fcd0534700701e1f87420bdf
reimerbo15 <bogdan.reimer@edu.fh-joannem.at>
2016-05-17 11:19:43 +0200

    added documentation

--- cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/PowerOfN.java
+++ cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/PowerOfN.java
@@ -3,6 +3,13 @@ package at.fhj.swd.confmana.calc.functions;
 import java.lang.Math;
 
 public class PowerOfN {
+	/**
+	 * The exponent of a base says how many times to use the number in a multiplication
+	 * @param first base 
+	 * @param power exponent
+	 * @return result of first^power
+	 */
+
 	public double powN(double first, double power){
 		return Math.pow(first, power);
 	}
