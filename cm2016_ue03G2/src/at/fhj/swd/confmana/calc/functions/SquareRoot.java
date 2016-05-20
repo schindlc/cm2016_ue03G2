0a45c6a6dbccba7c9039862ded3588f966595ff5
Nevzad Mujic <mujic.n@gmail.com>
2016-05-20 17:37:59 +0200

    sqrt method in CalculatorUtility implemented, throw exception if value < 0

--- cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/SquareRoot.java
+++ cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/SquareRoot.java
@@ -4,6 +4,9 @@ public class SquareRoot {
 	
 	// Calculates the square root from a double value
 	public double sqrt(double value){
+		if(value < 0)
+			throw new IllegalArgumentException();
+
 		return Math.sqrt(value);
 	}
 }
