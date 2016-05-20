341bffbb184ada3b418d8396e4ccb58b952d284f
Nevzad Mujic <mujic.n@gmail.com>
2016-05-20 16:02:41 +0200

    Comment added to sqrt method

--- cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/SquareRoot.java
+++ cm2016_ue03G2/src/at/fhj/swd/confmana/calc/functions/SquareRoot.java
@@ -1,6 +1,8 @@
 package at.fhj.swd.confmana.calc.functions;
 
 public class SquareRoot {
+	
+	// Calculates the square root from a double value
 	public double sqrt(double value){
 		return Math.sqrt(value);
 	}
